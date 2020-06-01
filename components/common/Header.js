dce341f4a9caba14081c87d2508b0a2b67258708
Thein Lwin Htun <theinlwin@blue-stone.net>
2020-06-01 19:09:20 +0630

    []

--- components/common/Header.js
+++ components/common/Header.js
@@ -5,28 +5,43 @@ import Link from 'next/link';
 const Header = props => (
   <div className="header">
     <div className="container">
-      <ul className="nav nav-tabs bg-dark">
-        <li className="nav-item">
-          <Link as="/instock-product" href='/instock-product'>
-            <a className="nav-link">Instock Product</a>
+      <nav className="navbar navbar-expand-lg py-3 navbar-dark bg-dark shadow-sm">
+        <div className="container">
+          <Link as="/" href='/'>
+            <a className="navbar-brand">
+              <img src="https://res.cloudinary.com/mhmd/image/upload/v1557368579/logo_iqjuay.png" width="45" alt="" className="d-inline-block align-middle mr-2"/>
+              <span className="text-uppercase font-weight-bold">Company</span>
+            </a>
           </Link>
-        </li>
-        <li className="nav-item">
-          <Link as="/customize-product" href='/customize-product'>
-            <a className="nav-link">Customize Product</a>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link as="/account" href='/account'>
-            <a className="nav-link">Account</a>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link as="/shopping-cart" href='/shopping-cart'>
-            <a className="nav-link">Shopping Cart</a>
-          </Link>
-        </li>
-      </ul>
+          <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" className="navbar-toggler"><span className="navbar-toggler-icon"></span></button>
+          <div id="navbarSupportedContent" className="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link as="/instock-product" href='/instock-product'>
+                  <a href="#" class="nav-link">Instock Product <span className="sr-only">(current)</span></a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link as="/customize-product" href='/customize-product'>
+                  <a className="nav-link">Customize Product</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link as="/account" href='/account'>
+                  <a className="nav-link">Account</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link as="/shopping-cart" href='/shopping-cart'>
+                  <a className="nav-link">
+                    <img src={require("../../assets/images/shopping-cart.png")}/>
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   </div>
 );
--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const withImages = require('next-images')
 const withSass = require('@zeit/next-sass');
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS(withSass());
+const withPlugins = require("next-compose-plugins");
+
+module.exports = withPlugins([
+  withImages,
+  withCSS(withSass())
+]);
